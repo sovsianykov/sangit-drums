12914dc1ba0c89bb116953a1ce3c359fe871acc3
”sovsianykov” <swebproger@gmail.com>
2021-03-14 13:34:12 +0200

    cardsGrid

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,26 +1,26 @@
 import React                       from "react";
-import TextTransition, { presets } from "react-text-transition";
+// import TextTransition, { presets } from "react-text-transition";
 import '../../index.scss'
 import {Grid} from "@material-ui/core";
-const TEXTS = [
-    "               ",
-    "Oсознанная игра "
-
-
-
-
-];
+// const TEXTS = [
+//     "               ",
+//     "Oсознанная игра "
+//
+//
+//
+//
+// ];
 
 const Home = () => {
-    const [index, setIndex] = React.useState(0);
-
-    React.useEffect(() => {
-        const intervalId = setInterval(() =>
-                setIndex(index => index + 1),
-            7000 // every 3 seconds
-        );
-        return () => clearTimeout(intervalId);
-    }, []);
+    // const [index, setIndex] = React.useState(0);
+    //
+    // React.useEffect(() => {
+    //     const intervalId = setInterval(() =>
+    //             setIndex(index => index + 1),
+    //         7000 // every 3 seconds
+    //     );
+    //     return () => clearTimeout(intervalId);
+    // }, []);
 
     return (
         <Grid container   className='hero' >
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
-import { Box } from '@material-ui/core';
-class Projects extends Component {
-    render() {
-        return (
-            <Box>
-                Projects
-            </Box>
-        );
-    }
-}
-export default Projects
+
+import * as React from 'react';
+import PageCard from '../../componens/PpageCard'
+import {Box,Grid} from "@material-ui/core";
+import song from '../../assets/audio/song.mp3'
+import {useEffect} from "react";
+
+const audio = new Audio(song)
+export default function Projects() {
+    useEffect(() =>{
+        audio.play()
+    },[])
+    const pages  = [
+        {title : "Кто такой Сергей Табунщик" , src : ""  },
+        {title : "О проекте" , src : ""  },
+        {title : "Барабанная Йога" , src : ""  },
+        {title : "Музыка" , src : ""  },
+        {title : "Видео" , src : ""  },
+        {title : "Контакты" , src : ""  }
+    ,]
+    return (
+        <Box  >
+            <Grid id='list'  container  >
+                {pages.map((card,i) => {
+                    return (
+                        <Grid item xs={12} sm = {4} key={i} className='cardGridItem'  >
+                            <PageCard  pages={card}  />
+                        </Grid>
+                    )
+                })}
+            </Grid>
+
+
+        </Box>
+    );
+};
