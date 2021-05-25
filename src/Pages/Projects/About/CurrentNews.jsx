89ecffc76e57c394ae2c0ff4483cefb5ed80f720
”sovsianykov” <swebproger@gmail.com>
2021-05-25 18:39:29 +0300

    news&Video

--- src/Pages/Projects/About/CurrentNews.jsx
+++ src/Pages/Projects/About/CurrentNews.jsx
@@ -16,22 +16,22 @@ const CurrentNews = () => {
                     <Grid item xs={12} sm={11} md={11}>
                         <Paper className={classes.paper}>
                             <Typography variant="h4" className={classes.title}>
-                                Открытые прослушивания
+                                Мастер-класс
                             </Typography>
                             <Grid container spacing={4} className={classes.edBlock}>
                                 <Grid item xs={12} sm={3} md={3}>
                                     <div className={classes.image}>
-                                        <img className={classes.pic} src={news1} alt="ava2" />
+                                        <img className={classes.pic} src={news4} alt="ava2" />
                                     </div>
                                 </Grid>
                                 <Grid item xs={12} sm={8} md={8}>
                                     <Typography variant='body1' className={classes.textSec}>
-                                        Друзья! 6 февраля 2021<br/>
-
-                                        Приглашаю на открытые прослушивания всех, кто желает научиться играть на барабанах,
-                                        хочет соприкоснуться с инструментом, попробовать себя в этом деле!<br/>
-
-                                        Это индивидуальные пробные уроки (до 20 минут), на которых мы познакомимся с вами и вы сможете попробовать сыграть на барабанах простейшие ритмические рисунки
+                                        Друзья,<br/>
+                                        29 мая приглашаю на мой Мастер-класс!<br/>
+                                        1 часть - для барабанщиков!<br/>
+                                        «Техника игры на барабанах» - основные приемы развития рук и ног<br/>
+                                        2 часть - для  инструменталистов, вокалистов, преподавателей!<br/>
+                                        «Ритмичная игра» - ощущение времени, как правильно заниматься с метрономом
                                     </Typography>
                                     <Typography
                                         variant="body1"
@@ -39,25 +39,52 @@ const CurrentNews = () => {
                                         className={classes.text}
                                         align="left"
                                     >
+                                        12.00-14.00<br/>
+                                        Рок школа<br/>
+                                        г. Бровары, б-р Независимости,16<br/>
+                                        Запись по тел. 097 9378986, 066 3791039<br/>
+                                        Количество мест ограничено!<br/>
+                                        Вход свободный
 
-                                        Запишитесь на пробный урок! <br/>
 
-                                        12.00 - 15.00<br/>
-
-                                        Студия «Eurovox»<br/>
-
-                                        ул. Межигірська, 87А,<br/>
-
-                                        ст. метро Т. Шевченко<br/>
-
-                                        КИЕВ<br/>
-
-                                        Вход свободный<br/>
-
-                                        Жду Вас!<br/>
                                     </Typography>
                                 </Grid>
                             </Grid>
+
+                            <Typography variant="h4" className={classes.title}>
+                                Ритмичная игра
+                            </Typography>
+                            <Grid container spacing={4} className={classes.edBlock}>
+                                <Grid item xs={12} sm={3} md={3}>
+                                    <div className={classes.image}>
+                                        <img className={classes.pic} src={news3} alt="ava2" />
+                                    </div>
+                                </Grid>
+                                <Grid item xs={12} sm={8} md={8}>
+                                    <Typography variant='body1' className={classes.textSec}>
+                                        3 апреля<br/>
+                                        Тренинг- «Ритмичная игра»<br/>
+                                        Для всех инструменталистов, вокалистов и преподавателей.<br/>
+                                        Научись играть ритмично!<br/>
+                                        Друзья, 3 апреля я провожу в Киеве свой первый авторский тренинг «Ритмичная игра».<br/>
+                                        Приглашаю всех желающих, кто хотел бы улучшить своё чувство ритма, времени, научиться более качественно исполнять музыкальные ритмические структуры. Метод и непосредственная проработка упражнений на тренинге позволят вам понять суть ритмичной игры и получить собственное представление о том, как сделать свою игру более качественной, ритмичной и драйвовой. Это воспитание внутреннего чувства, ощущения того, что в музыкальных кругах называют «Good time» - хорошее время.
+                                    </Typography>
+                                    <Typography
+                                        variant="body1"
+                                        paragraph
+                                        className={classes.text}
+                                        align="left"
+                                    >
+                                        12.00 - 14.00<br/>
+                                        Киев<br/>
+                                        Студия «Eurovox»<br/>
+                                        ул. Межигорская, 87А,<br/>
+                                        ст. метро Т. Шевченко
+
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+
                             <Typography variant="h4" className={classes.title}>
                                 Йога барабанщика
                             </Typography>
@@ -93,22 +120,25 @@ const CurrentNews = () => {
                                     </Typography>
                                 </Grid>
                             </Grid>
+
                             <Typography variant="h4" className={classes.title}>
-                                Ритмичная игра
+                                Открытые прослушивания
                             </Typography>
                             <Grid container spacing={4} className={classes.edBlock}>
                                 <Grid item xs={12} sm={3} md={3}>
                                     <div className={classes.image}>
-                                        <img className={classes.pic} src={news3} alt="ava2" />
+                                        <img className={classes.pic} src={news1} alt="ava2" />
                                     </div>
                                 </Grid>
                                 <Grid item xs={12} sm={8} md={8}>
                                     <Typography variant='body1' className={classes.textSec}>
-                                        3 апреляТренинг- «Ритмичная игра»<br/>
-                                        Для всех инструменталистов, вокалистов и преподавателей.<br/>
-                                        Научись играть ритмично!<br/>
-                                        Друзья, 3 апреля я провожу в Киеве свой первый авторский тренинг «Ритмичная игра».<br/>
-                                        Приглашаю всех желающих, кто хотел бы улучшить своё чувство ритма, времени, научиться более качественно исполнять музыкальные ритмические структуры. Метод и непосредственная проработка упражнений на тренинге позволят вам понять суть ритмичной игры и получить собственное представление о том, как сделать свою игру более качественной, ритмичной и драйвовой. Это воспитание внутреннего чувства, ощущения того, что в музыкальных кругах называют «Good time» - хорошее время.
+                                        Друзья!<br/>
+                                        6 февраля 2021<br/>
+
+                                        Приглашаю на открытые прослушивания всех, кто желает научиться играть на барабанах,
+                                        хочет соприкоснуться с инструментом, попробовать себя в этом деле!<br/>
+
+                                        Это индивидуальные пробные уроки (до 20 минут), на которых мы познакомимся с вами и вы сможете попробовать сыграть на барабанах простейшие ритмические рисунки
                                     </Typography>
                                     <Typography
                                         variant="body1"
@@ -116,46 +146,22 @@ const CurrentNews = () => {
                                         className={classes.text}
                                         align="left"
                                     >
-                                        12.00 - 14.00<br/>
-                                        Киев<br/>
+
+                                        Запишитесь на пробный урок! <br/>
+
+                                        12.00 - 15.00<br/>
+
                                         Студия «Eurovox»<br/>
-                                        ул. Межигорская, 87А,<br/>
-                                        ст. метро Т. Шевченко
 
-                                    </Typography>
-                                </Grid>
-                            </Grid>
-                            <Typography variant="h4" className={classes.title}>
-                                Мастер-класс
-                            </Typography>
-                            <Grid container spacing={4} className={classes.edBlock}>
-                                <Grid item xs={12} sm={3} md={3}>
-                                    <div className={classes.image}>
-                                        <img className={classes.pic} src={news4} alt="ava2" />
-                                    </div>
-                                </Grid>
-                                <Grid item xs={12} sm={8} md={8}>
-                                    <Typography variant='body1' className={classes.textSec}>
-                                        Друзья, 29 мая приглашаю на мой Мастер-класс!<br/>
-                                        1 часть - для барабанщиков!<br/>
-                                        «Техника игры на барабанах» - основные приемы развития рук и ног<br/>
-                                        2 часть - для  инструменталистов, вокалистов, преподавателей!<br/>
-                                        «Ритмичная игра» - ощущение времени, как правильно заниматься с метрономом
-                                    </Typography>
-                                    <Typography
-                                        variant="body1"
-                                        paragraph
-                                        className={classes.text}
-                                        align="left"
-                                    >
-                                        12.00-14.00<br/>
-                                        Рок школа<br/>
-                                        г. Бровары, б-р Независимости,16<br/>
-                                        Запись по тел. 097 9378986, 066 3791039<br/>
-                                        Количество мест ограничено!<br/>
-                                        Вход свободный
+                                        ул. Межигірська, 87А,<br/>
 
+                                        ст. метро Т. Шевченко<br/>
 
+                                        КИЕВ<br/>
+
+                                        Вход свободный<br/>
+
+                                        Жду Вас!<br/>
                                     </Typography>
                                 </Grid>
                             </Grid>
